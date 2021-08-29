1a4189066ee40bb1d2b05e5adeaafba69d0750ad
Ting's Mac <huxu@uwplatt.edu>
2021-08-29 13:21:55 -0500

    Added draggable feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,5 +155,58 @@ $("#remove-tasks").on("click", function () {
   saveTasks();
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    // loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function () {
+        var text = $(this).find("p").text().trim();
+
+        var date = $(this).find("span").text().trim();
+
+        console.log(text, date);
+
+        // trim down list's ID to match object property
+        var arrName = $(this).attr("id").replace("list-", "");
+
+        // update array on tasks object and save
+        tasks[arrName] = tempArr;
+        saveTasks();
+      });
+  },
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function (event, ui) {
+    console.log("over");
+  },
+  out: function (event, ui) {
+    console.log("out");
+  },
+});
+
 // load tasks for the first time
 loadTasks();
